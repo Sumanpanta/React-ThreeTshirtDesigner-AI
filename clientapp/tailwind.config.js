1c5b70015212217c5501fd8abd036feccae7df94
masterrujino47@gmail.com <masterrujino47@gmail.com>
2024-06-09 12:24:37 +0545

    initial commit

---
+++ clientapp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content vitra "./index.html aru aru kura haru "
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
