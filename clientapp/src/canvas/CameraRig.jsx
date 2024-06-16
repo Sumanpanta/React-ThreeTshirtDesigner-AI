42a0f0dfd2d9f940243594f210bc984c7aec4999
masterrujino47@gmail.com <masterrujino47@gmail.com>
2024-06-16 12:42:07 +0545

    camerRig fxn added: viewing angle haru

--- clientapp/src/canvas/CameraRig.jsx
+++ clientapp/src/canvas/CameraRig.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import {useFrame } from '@react-three/fiber';
 import {easing } from 'maath';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { Group } from 'three';
 
-const CameraRig = () => {
+const CameraRig = ({children }) => {
+  // 
+  const group = useRef();
   const snap = useSnapshot(store);
+
+
+  useFrame(()=> {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    //Lets set the initial position of the model, for that we use
+    let targetPosition = [-0.4, 0, 2];
+
+    if(snap.intro){
+      if(isBreakpoint) targetPosition = [0,0,2];
+      if(isMobile) targetPosition = [0,0.2,2.5];
+
+    } else{
+      if(isMobile) targetPosition = [0,0,2.5];
+      else targetPosition = [0,0,2];
+    }
+    //set model camera position 
+    easing.damp3(
+      state.camera.position, 
+      targetPosition,
+      0.25,
+      delta
+    )
+
+  // set the model rotation smoothly.
+  easing.dampE(
+    group.current.rotation,
+    [state.pointer.y / 10, -state.pointer.x/5, 0],
+    0.25,
+    delta
+  )
+  })
+
+    
+
   return (
-    <div>CameraRig</div>
+   <group ref={group}>{children}</group>
   )
 }
 
 export default CameraRig
 
-//positioning or viewing angle of the camera for tshirt
+//positioning or viewing angle of the camera for tshirt, see docs.
