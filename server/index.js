9e1fd98eb3ab0b50c9c3bb08cce6064836ee2edd
masterrujino47@gmail.com <masterrujino47@gmail.com>
2024-06-18 12:47:41 +0545

    gensu.routes.js code updated

--- server/index.js
+++ server/index.js
@@ -2,13 +2,22 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import dalleRoutes from './routes/gensu.routes.js'       //tyo routes vanni dalleRoutes ho 
+
 dotenv.config();
 
 const app = express();         //setting up express application by running ..
 app.use(cors());                                           //set up the middleware by app.use command and need to pass the cors.//otherwise cors problems
 app.use(express.json({limit: '50mb'}))             //to specify the weight of the pillow begn sent, limitof 
 
+app.use('/api/v1/dalle', dalleRoutes);                                                      //have to use or consume that dalleRoutes import or tya vitra ko kura 
+
 app.get('/',(req, res) => {
     res.status(200).json({ message: "hello dude"})
 });           //demo.route by doing app.get run by forward slash
         // there we can simply res that status of 200 and then .json where can pass message hello dude
+
+app.listen(8080,() => 
+    console.log('Server has started on port 8080'))  
+                                            //run matrai vayo mathi start garera tara host vayena so using this
+                                        //kun port ma listen garni(host) vanera tyo 8080 deko, 5000 dina ni sakxam
---
+++ server/routes/gensu.routes.js
@@ -0,0 +1,41 @@
+import express, { response } from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const router = express.Router();                      //we can create so additional routes so we can specify routers, see docs(474-479  )
+ 
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+router.route('/').get((req, res) => {
+    res.status(200).json({ message: "Hello from DaLL.E Gensu ROUTES"})
+})                                      //lets create a couple of routes, see index.js(sabbai info xa)
+
+
+router.route('/'.post(async (req, res )=>{                   //see docs (488-492,496)
+    try {
+        const {prompt} = req.body;
+        const response = await openai.createImage({
+            prompt,
+            n:1,
+            size: '1024*1024',
+            response_format: 'b64_json'
+        });
+        const image = response.data.data[0].b64_json;
+        
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong "})
+        
+    }
+
+}))
+
+export default router;                    //have to export default router
+//we have to connect all this inside index.js(to show , where is this going to be., what is this route.)
